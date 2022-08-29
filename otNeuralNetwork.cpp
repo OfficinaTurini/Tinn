4d115cedcfe83c8637fc95a09a57be7f45ad1039
Manuele Turini <45485208+OfficinaTurini@users.noreply.github.com>
2022-08-29 14:57:34 +0200

    Add files via upload

---
+++ otNeuralNetwork.cpp
@@ -0,0 +1,303 @@
+#include "otNeuralNetwork.h"
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+otTinn::otTinn(int nips, int nops, int nhid)
+{
+	// otTinn only supports one hidden layer so there are two biases.
+	_nb = 2;
+	_nw = nhid * (nips + nops);
+	_w = (float *) calloc(_nw, sizeof(*_w));
+	_x = _w + nhid * nips;
+	_b = (float *) calloc(_nb, sizeof(*_b));
+	_h = (float *) calloc(nhid, sizeof(*_h));
+	_o = (float*) calloc(nops, sizeof(*_o));
+	_nips = nips;
+	_nhid = nhid;
+	_nops = nops;
+	wbrand();
+}
+
+otTinn::~otTinn()
+{
+	free(_w);
+	free(_b);
+	free(_h);
+	free(_o);
+}
+
+void otTinn::wbrand()
+{
+	for(int i = 0; i < _nw; i++) _w[i] = frand() - 0.5f;
+	for(int i = 0; i < _nb; i++) _b[i] = frand() - 0.5f;
+}
+
+float otTinn::frand()
+{
+	return rand() / (float) RAND_MAX;
+}
+
+float otTinn::pderr(const float a, const float b)
+{
+	return a - b;
+}
+
+float otTinn::toterr(const float * const tg, const float * const o, const int size)
+{
+	float sum = 0.0f;
+	for(int i = 0; i < size; i++)
+		sum += err(tg[i], o[i]);
+	return sum;
+}
+
+float otTinn::err(const float a, const float b)
+{
+	return 0.5f * (a - b) * (a - b);
+}
+
+float otTinn::act(const float a)
+{
+	return 1.0f / (1.0f + expf(-a));
+}
+
+float otTinn::pdact(const float a)
+{
+	return a * (1.0f - a);
+}
+
+void otTinn::bprop(const float * const in, const float * const tg, float rate)
+{
+	for(int i = 0; i < _nhid; i++)
+	{
+		float sum = 0.0f;
+		// Calculate total error change with respect to output.
+		for(int j = 0; j < _nops; j++)
+		{
+			const float a = pderr(_o[j], tg[j]);
+			const float b = pdact(_o[j]);
+			sum += a * b * _x[j * _nhid + i];
+			// Correct weights in hidden to output layer.
+			_x[j * _nhid + i] -= rate * a * b * _h[i];
+		}
+		// Correct weights in input to hidden layer.
+		for(int j = 0; j < _nips; j++)
+			_w[i * _nips + j] -= rate * sum * pdact(_h[i]) * in[j];
+	}
+}
+
+void otTinn::fprop(const float * const in)
+{
+	// Calculate hidden layer neuron values.
+	for(int i = 0; i < _nhid; i++)
+	{
+		float	sum = 0.0f;
+		for(int j = 0; j < _nips; j++)
+			sum += in[j] * _w[i * _nips + j];
+		_h[i] = act(sum + _b[0]);
+	}
+	// Calculate output layer neuron values.
+	for(int i = 0; i < _nops; i++)
+	{
+		float	sum = 0.0f;
+		for(int j = 0; j < _nhid; j++)
+			sum += _h[j] * _x[i * _nhid + j];
+		_o[i] = act(sum + _b[1]);
+	}
+}
+
+float * otTinn::predict(const float * in)
+{
+	fprop(in);
+	return _o;
+}
+
+float otTinn::train(const float * in, const float * tg, float rate)
+{
+	fprop(in);
+	bprop(in, tg, rate);
+	return toterr(tg, _o, _nops);
+}
+
+bool otTinn::save(const char * path)
+{
+	FILE	* const file = fopen(path, "w");
+	if(file == NULL)
+		return false;
+	// Save header.
+	fprintf(file, "%d %d %d\n", _nips, _nhid, _nops);
+	// Save biases and weights.
+	for(int i = 0; i < _nb; i++) fprintf(file, "%f\n", (double) _b[i]);
+	for(int i = 0; i < _nw; i++) fprintf(file, "%f\n", (double) _w[i]);
+	fclose(file);
+	return true;
+}
+
+bool otTinn::load(const char * const path)
+{
+	FILE	* const file = fopen(path, "r");
+	if(file == NULL)
+		return false;
+	_nips = 0;
+	_nhid = 0;
+	_nops = 0;
+	// Load header.
+	fscanf(file, "%d %d %d\n", &_nips, &_nhid, &_nops);
+	
+	free(_w);
+	free(_b);
+	free(_h);
+	free(_o);
+
+	// Build a new otTinn.
+	_nw = _nhid * (_nips + _nops);
+	_w = (float *) calloc(_nw, sizeof(*_w));
+	_x = _w + _nhid * _nips;
+	_b = (float *) calloc(_nb, sizeof(*_b));
+	_h = (float *) calloc(_nhid, sizeof(*_h));
+	_o = (float *) calloc(_nops, sizeof(*_o));
+	// Load bias and weights.
+	for(int i = 0; i < _nb; i++) fscanf(file, "%f\n", &_b[i]);
+	for(int i = 0; i < _nw; i++) fscanf(file, "%f\n", &_w[i]);
+	fclose(file);
+	return true;
+}
+
+void otTinn::print(const float * arr, const int size)
+{
+	for(int i = 0; i < size; i++)
+		printf("%f ", (double) arr[i]);
+	printf("\n");
+}
+
+otNeuralFramework::otNeuralFramework(int nips, int nops, int nhid)
+{
+	_nips = nips;
+	_nops = nops;
+	_nhid = nhid;
+	_in = 0;
+	_tg = 0;
+	_rows = -1;
+	_error = 0.0f;
+}
+
+otNeuralFramework::~otNeuralFramework()
+{
+}
+
+float ** otNeuralFramework::new2d(const int rows, const int cols)
+{
+	float	** row = (float **) malloc((rows) * sizeof(float *));
+	for(int r = 0; r < rows; r++)
+		row[r] = (float *) malloc((cols) * sizeof(float));
+	return row;
+}
+
+int otNeuralFramework::lines(FILE * const file)
+{
+	int	ch = EOF;
+	int lines = 0;
+	int pc = '\n';
+	while((ch = getc(file)) != EOF)
+	{
+		if(ch == '\n')
+			lines++;
+		pc = ch;
+	}
+	if(pc != '\n')
+		lines++;
+	rewind(file);
+	return lines;
+}
+
+char * otNeuralFramework::readln(FILE * const file)
+{
+	int		ch = EOF;
+	int		reads = 0;
+	int		size = 1024;
+	char	* line = (char *) malloc((size) * sizeof(char));
+	while((ch = getc(file)) != '\n' && ch != EOF)
+	{
+		line[reads++] = ch;
+		if(reads + 1 == size)
+			line = (char *) realloc((line), (size *= 2) * sizeof(char));
+	}
+	line[reads] = '\0';
+	return line;
+}
+
+void otNeuralFramework::parse(char * line, const int row)
+{
+	const int cols = _nips + _nops;
+	for(int col = 0; col < cols; col++)
+	{
+		const float val = (float) atof(strtok(col == 0 ? line : NULL, " "));
+		if(col < _nips)
+			_in[row][col] = val;
+		else
+			_tg[row][col - _nips] = val;
+	}
+}
+
+bool otNeuralFramework::dataset(const char * path)
+{
+	bool	ret = false;
+	FILE	* file = fopen(path, "r");
+	if(file == NULL)
+		return ret;
+	_rows = lines(file);
+
+	if(_rows >= 1)
+	{
+		_in = new2d(_rows, _nips);
+		_tg = new2d(_rows, _nops);
+
+		if(_in && _tg)
+		{
+			for(int row = 0; row < _rows; row++)
+			{
+				char	* line = readln(file);
+				parse(line, row);
+				free(line);
+			}
+			fclose(file);
+			ret = true;
+		}
+	}
+	return ret;
+}
+
+void otNeuralFramework::dfree()
+{
+	for(int row = 0; row < _rows; row++)
+	{
+		free(_in[row]);
+		free(_tg[row]);
+	}
+	free(_in);
+	free(_tg);
+}
+
+void otNeuralFramework::shuffle()
+{
+	for(int a = 0; a < _rows; a++)
+	{
+		const int b = rand() % _rows;
+		float	* ot = _tg[a];
+		float	* it = _in[a];
+		// Swap output.
+		_tg[a] = _tg[b];
+		_tg[b] = ot;
+		// Swap input.
+		_in[a] = _in[b];
+		_in[b] = it;
+	}
+}
+
+float otNeuralFramework::training(otTinn & tin, float rate)
+{
+	_error = 0;
+	for(int j = 0; j < _rows; j++)
+		_error += tin.train(_in[j], _tg[j], rate);
+	return _error;
+}
---
+++ otNeuralNetwork.h
@@ -0,0 +1,110 @@
+#pragma once
+
+#include <stdio.h>
+/*! Copyright 2017-2022 Officina Turini\n
+This file is part of otNeuralNetwork project.\n
+
+otStudio is free software: you can redistribute it and/or modify\n
+it under the terms of the GNU General Public License as published by\n
+the Free Software Foundation, either version 3 of the License.n
+\n
+otStudio is distributed in the hope that it will be useful,\n
+but WITHOUT ANY WARRANTY; without even the implied warranty of\n
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n
+GNU General Public License for more details.\n
+\n
+You should have received a copy of the GNU General Public License\n
+along with otNeuralNetwork.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#if defined(_WIN32) || defined(__WIN32__)
+	#if defined(_MSC_VER)
+		#ifdef OTNN_STATIC_LIBRARY
+			#define OTNN_LIB
+		#else
+			#ifdef OTNN_SOURCE_CODE
+				#define OTNN_LIB __declspec(dllexport)
+			#else
+				#define OTNN_LIB __declspec(dllimport)
+			#endif
+		#endif
+	#else
+	#if defined(__BORLANDC__)
+		#ifdef OTNN_SOURCE_CODE
+			#define OTNN_LIB _export
+		#else
+			#define OTNN_LIB _import
+		#endif
+	#endif
+	#endif
+#else
+// UNIX
+
+#endif
+
+class OTNN_LIB otTinn
+{
+public:
+	otTinn(int nips, int nops, int nhid);
+	~otTinn();
+
+	float			* predict(const float * in);	///< Returns an output prediction given an input.
+	float			train(const float * in, const float * tg, float rate);	///< Trains a otTinn with an input and target output with a learning rate. Returns target to output error.
+	bool			save(const char * path);		///< Saves a otTinn to disk.
+	bool			load(const char * path);		///< Loads a otTinn from disk.
+	void			print(const float * arr, const int size);	///< Prints an array of floats. Useful for printing predictions.
+
+private:
+	void			wbrand();	///< Randomizes otTinn weights and biases.
+	float			frand();	///< Returns floating point random from 0.0 - 1.0.
+	inline float	err(const float a, const float b);		///< Computes error.
+	inline float	pderr(const float a, const float b);	///< Returns partial derivative of error function.
+	float			toterr(const float * const tg, const float * const o, const int size);	///< Computes total error of target to output.
+	float			act(const float a);						///< Activation function.
+	float			pdact(const float a);					///< Returns partial derivative of activation function.
+	void			bprop(const float * const in, const float * const tg, float rate);	///< Performs back propagation.
+	void			fprop(const float * const in);			///< Performs forward propagation.
+
+	float	* _w;	///< All the weights.
+	float	* _x;	///< Hidden to output layer weights.
+	float	* _b;	///< Biases.
+	float	* _h;	///< Hidden layer.
+	float	* _o;	///< Output layer.
+	int		_nb;	///< Number of biases - always two - otTinn only supports a single hidden layer.
+	int		_nw;	///< Number of weights.
+	int		_nips;	///< Number of inputs.
+	int		_nhid;	///< Number of hidden neurons.
+	int		_nops;	///< Number of outputs.
+};
+
+class OTNN_LIB otNeuralFramework
+{
+public:
+	otNeuralFramework(int nips, int nops, int nhid);
+	~otNeuralFramework();
+
+	bool	dataset(const char * path);
+	void	shuffle();
+	float	training(otTinn & tin, float rate);
+	float	error() { return _error; }
+	int		rows() { return _rows; }
+	int		outputs() { return _nops; }
+	const float	* input(int row = 0) { return _in[row]; }
+	const float	* target(int row = 0) { return _tg[row]; }
+
+private:
+	float	** new2d(const int rows, const int cols);	///< Create new 2D array of floats.
+	int		lines(FILE * const file);					///< Returns the number of lines in a file.
+	char	* readln(FILE * const file);				///< Reads a line from a file.
+	void	parse(char * line, const int row);			///< Gets one row of inputs and outputs from a string.
+	void	dfree();									///< Frees a data object from the heap.
+
+	int		_nips;	///< Number of inputs to neural network.
+	int		_nops;	///< Number of outputs to neural network.
+	int		_nhid;	///< Number of hidden layers
+	int		_rows;	///< Rows in training file (number of sets for neural network).
+	float	** _in;	///< 2D floating point array of input.
+	float	** _tg;	///< 2D floating point array of target.
+	float	_error;
+};
+
